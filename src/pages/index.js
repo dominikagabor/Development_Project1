26a382aa205cebc15f6629e3839146709a15804f
Dominika Gabor <dominikagabor190@gmail.com>
2019-12-03 19:25:13 +0100

    ver1

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,10 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="xxxxxxxxxxxxx" />
+    <h1>rgdg</h1>
+    <p>drgre.</p>
+    <p>drgdrgdrgdrat.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,9 +6,9 @@ import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <SEO title="Pagsdsd" />
+    <h1>str druda</h1>
+    <p>ggggg</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
